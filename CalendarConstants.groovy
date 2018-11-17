e84997c952b8092e72c079f1283a1aca6ed853ac
Andy Miller <onetribeyoyo@gmail.com>
2018-11-17 14:17:31 -0600

    simplified school constants.

--- CalendarConstants.groovy
+++ CalendarConstants.groovy
@@ -4,7 +4,5 @@ class CalendarConstants {
     static final def MOM = "mom" // css class for nights spent with Mom
 
     static final def NO_SCHOOL = "no-school" // css class for days with no school
-    static final def NO_SPPS   = "no-spps"   // css class for days with no spps
-    static final def NO_UMA    = "no-uma"    // css class for days with no uma
 
 }
