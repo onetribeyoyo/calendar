f4c263a0ead5b2d043a5f0f371a9ba87640ce0df
Andy Miller <onetribeyoyo@gmail.com>
2019-09-02 20:36:47 -0500

    new style for split days.

--- CalendarConstants.groovy
+++ CalendarConstants.groovy
@@ -3,6 +3,8 @@ class CalendarConstants {
     static final def DAD = "dad" // css class for nights spent with Dad
     static final def MOM = "mom" // css class for nights spent with Mom
 
+    static final def SPLIT = "split" // css class for nights split between both parents
+
     static final def NO_SCHOOL = "no-school" // css class for days with no school
 
 }
